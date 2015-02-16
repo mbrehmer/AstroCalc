2628537a44b64205f73e037b1eb0e96c0e202d2d
mbrehmer <github@mbrehmer.de>
2015-02-16 15:35:28 +0100

    Add DateOfEaster() with Unit Test

--- AstroCalc/DateAndTime.cs
+++ AstroCalc/DateAndTime.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace AstroCalc
 {
-    public class DateAndTime
+    public static class DateAndTime
     {
+		 /// <summary>
+		 /// Calculates the date of Easter for a given year.
+		 /// </summary>
+		 /// <param name="date">Date and Time for the calculation</param>
+		 /// <returns>The date of Ester</returns>
+		 public static DateTime DateOfEaster(int year)
+		 {
+			 int a = year % 19;
+			 double b = Math.Truncate(year / 100.0); // Who doesn't Math.Truncate() return an Integer ???
+			 int c = year % 100;
+			 double d = Math.Truncate(b / 4.0);
+			 int e = Convert.ToInt32(b) % 4;
+			 double f = Math.Truncate((b + 8) / 25.0);
+			 double g = Math.Truncate((b - f + 1.0) / 3.0);
+			 int h = (19*a + Convert.ToInt32(b) - Convert.ToInt32(d) - Convert.ToInt32(g) + 15) % 30;
+			 double i = Math.Truncate(c / 4.0);
+			 int k = c % 4;
+			 int l = (32 + 2*e + 2*Convert.ToInt32(i) - h - k) % 7;
+			 double m = Math.Truncate((a + 11*h + 22*l) / 451.0);
+			 double n = Math.Truncate((h + l - 7*m + 114) / 31.0);
+			 int p = (h + l - 7*Convert.ToInt32(m) + 114) % 31;
+
+			 DateTime DateOfEaster = new DateTime(year, Convert.ToInt32(n), p+1);
+			 return DateOfEaster;
+		 }
     }
 }
---
+++ UnitTests/DateAndTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AstroCalc;
+
+namespace AstroCalc.UnitTests
+{
+	[TestClass]
+	public class DatesAndTimes
+	{
+		[TestMethod]
+		public void DateOfEaster2009()
+		{
+			DateTime date = AstroCalc.DateAndTime.DateOfEaster(2009);
+			DateTime easter2009 = new DateTime(2009, 4, 12);
+
+			Assert.AreEqual(date.ToShortDateString(), easter2009.ToShortDateString());
+			Assert.AreEqual(date.ToShortTimeString(), easter2009.ToShortTimeString());
+		}
+	}
+}
