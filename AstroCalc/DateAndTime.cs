18de5318c095252a0721a55c90e2e7d4b65ffe4b
mbrehmer <github@mbrehmer.de>
2015-04-22 19:58:46 +0200

    Fix tabstop issues

--- AstroCalc/DateAndTime.cs
+++ AstroCalc/DateAndTime.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AstroCalc
 {
 	/// <summary>
 	/// This class provides the funktions for dates and times
 	/// </summary>
-   public static class DateAndTime
-   {
+	public static class DateAndTime
+	{
 		/// <summary>
 		/// Calculates the date of Easter for a given year after 1582
 		/// </summary>
@@ -29,15 +25,15 @@ namespace AstroCalc
 			int e = Convert.ToInt32(b) % 4;
 			double f = Math.Truncate((b + 8) / 25.0);
 			double g = Math.Truncate((b - f + 1.0) / 3.0);
-			int h = (19*a + Convert.ToInt32(b) - Convert.ToInt32(d) - Convert.ToInt32(g) + 15) % 30;
+			int h = (19 * a + Convert.ToInt32(b) - Convert.ToInt32(d) - Convert.ToInt32(g) + 15) % 30;
 			double i = Math.Truncate(c / 4.0);
 			int k = c % 4;
-			int l = (32 + 2*e + 2*Convert.ToInt32(i) - h - k) % 7;
-			double m = Math.Truncate((a + 11*h + 22*l) / 451.0);
-			double n = Math.Truncate((h + l - 7*m + 114) / 31.0);
-			int p = (h + l - 7*Convert.ToInt32(m) + 114) % 31;
+			int l = (32 + 2 * e + 2 * Convert.ToInt32(i) - h - k) % 7;
+			double m = Math.Truncate((a + 11 * h + 22 * l) / 451.0);
+			double n = Math.Truncate((h + l - 7 * m + 114) / 31.0);
+			int p = (h + l - 7 * Convert.ToInt32(m) + 114) % 31;
 
-			DateTime DateOfEaster = new DateTime(year, Convert.ToInt32(n), p+1);
+			DateTime DateOfEaster = new DateTime(year, Convert.ToInt32(n), p + 1);
 			return DateOfEaster;
 		}
 	}
